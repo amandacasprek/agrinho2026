2f5e583f8434a82c5ab452e827af171fcc28f910
amandacasprek <amanda.casprek@escola.pr.gov.br>
2026-06-10 09:33:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+function calcularEconomia(){
+
+```
+let litros = Number(document.getElementById("litros").value);
+
+if(litros <= 0 || isNaN(litros)){
+    document.getElementById("resultado").innerHTML =
+    "Digite um valor válido.";
+    return;
+}
+
+let economiaAnual = litros * 365;
+
+document.getElementById("resultado").innerHTML =
+"Economia estimada em um ano: <strong>" +
+economiaAnual.toLocaleString('pt-BR') +
+" litros de água.</strong>";
+```
+
+}
 
